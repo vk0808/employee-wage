e4b7a6b20a4753d882cb404738c9bd3dae05c595
VINOD KUMAR R <ravivinod21@gmail.com>
2021-08-02 23:04:50 +0530

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
