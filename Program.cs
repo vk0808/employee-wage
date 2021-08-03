0d47033927b7138085a73e8e5dfa15adb3a7cb8a
vk0808 <vk4765712@gmail.com>
2021-08-03 14:31:02 +0530

    feat: calculate partTime wage

--- Program.cs
+++ Program.cs
@@ -9,23 +9,31 @@ namespace EmployeeWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation");
 
             /// Variables
-            int IS_PRESENT = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int WAGE_PER_HR = 1000;
-            int hoursWorked = 8;
-            int dailyWage = 0;
+            int hoursWorked;
+            int dailyWage;
 
             Random rand = new Random();
-            int empCheck = rand.Next(0, 2);
+            int empCheck = rand.Next(0, 3);
 
-            if (empCheck == IS_PRESENT)
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
-                dailyWage = WAGE_PER_HR * hoursWorked;
+                Console.WriteLine("Employee is Present Full-time");
+                hoursWorked = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Present Part-time");
+                hoursWorked = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                hoursWorked = 0;
             }
+            dailyWage = WAGE_PER_HR * hoursWorked;
             Console.WriteLine("Total Wage is " + dailyWage);
         }
     }
