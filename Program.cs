16730f81f253ab19d66642a867e6056bd434419c
vk0808 <vk4765712@gmail.com>
2021-08-03 14:03:48 +0530

    feat: employee check

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            /// Variables
+            int IS_PRESENT = 1;
+
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
