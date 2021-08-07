c7a34b873c85c910c24602d739d7987fc97202d0
vk0808 <vk4765712@gmail.com>
2021-08-07 21:51:00 +0530

    added: class and object

---
+++ Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+
+    public class Employee
+    {
+        int WAGE_PER_HR;
+        int MAX_WORK_HRS;
+        int MAX_WORK_DAYS;
+
+        int FULL_TIME_HR = 8;
+        int HALF_TIME_HR = 4;
+        int ABSENT = 0;
+
+        int totalWorkHr = 0;
+        int day = 1;
+        int wage = 0;
+
+
+        public Employee(int wagePerHr, int maxWorkHrs, int maxWorkDays)
+        {
+            this.WAGE_PER_HR = wagePerHr;
+            this.MAX_WORK_HRS = maxWorkHrs;
+            this.MAX_WORK_DAYS = maxWorkDays;
+        }
+
+        public string Greet()
+        {
+            return "Welcome to Employee Wage Computation";
+        }
+
+        public void CalculateWage()
+        {
+            while (day < this.MAX_WORK_DAYS && totalWorkHr < this.MAX_WORK_HRS)
+            {
+                Random rand = new Random();
+                
+                /// Determine if the employee is half/full/absent using random object 
+                int empCheck = rand.Next(0, 3);
+
+                /// Determine hours worked based on half/full/absent using switch and store it in hoursWorked
+                var hoursWorked = empCheck switch
+                {
+                    1 => HALF_TIME_HR,
+                    2 => FULL_TIME_HR,
+                    _ => ABSENT,
+                };
+
+                /// Calculate total working hour
+                totalWorkHr += hoursWorked;
+
+                /// Calculate total wage
+                wage = (this.WAGE_PER_HR * hoursWorked) + wage;
+
+                /// Increment day
+                day++;
+            }
+        }
+
+        public int GetDays()
+        {
+            return day;
+        }
+
+        public int GetHrs()
+        {
+            return totalWorkHr;
+        }
+
+        public int GetWage()
+        {
+            return wage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,50 +6,16 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
+            Employee employee = new Employee(1000, 100, 20);
 
-            /// Variables
-            /// int IS_PART_TIME = 1;
-            /// int IS_FULL_TIME = 2;
-            int WAGE_PER_HR = 1000;
-            int MAX_WORK_DAYS = 20;
-            int MAX_WORK_HRS = 100;
-            int totalWorkHr = 0;
-            int day = 1;
-            int hoursWorked;
-            int wage = 0;
 
-            while (day < MAX_WORK_DAYS || totalWorkHr < MAX_WORK_HRS)
-            {
+            Console.WriteLine(employee.Greet() + "\n");
 
-                Random rand = new Random();
-                int empCheck = rand.Next(0, 3);
+            employee.CalculateWage();
 
-                switch (empCheck)
-                {
-                    case 1:
-                        hoursWorked = 4;
-                        break;
-
-                    case 2:
-                        hoursWorked = 8;
-                        break;
-
-                    default:
-                        hoursWorked = 0;
-                        break;
-                }
-                /// Calculate total working hour
-                totalWorkHr = totalWorkHr + hoursWorked;
-
-                /// Calculate total wage
-                wage = (WAGE_PER_HR * hoursWorked) + wage;
-
-                day++;
-            }
-            Console.WriteLine("Total Working Hours is " + totalWorkHr);
-            Console.WriteLine("No of Working days is " + day);
-            Console.WriteLine("Total Wage is " + wage);
+            Console.WriteLine("Total Working Hours: " + employee.GetHrs());
+            Console.WriteLine("No of Working days: " + employee.GetDays());
+            Console.WriteLine("Total Wage: " + employee.GetWage());
         }
     }
 }
